f1377d1a962351a88e63e3fbe6412d9629f605b8
motephyr <motephyr@gmail.com>
2015-10-26 15:52:09 +0800

    combine root.js to app.js

--- client/src/app.js
+++ client/src/app.js
@@ -1,5 +1,3 @@
-import Root from "./containers/Root";
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -7,7 +5,41 @@ import createBrowserHistory from 'history/lib/createBrowserHistory'
 
 const history = createBrowserHistory();
 
+import {Component, PropTypes} from 'react';
+import {Router, RouteHandler, IndexRoute, Route } from "react-router";
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
+
+import {Provider} from 'react-redux';
+import configureStore from './store/configureStore';
+import MainContainer from './containers/MainContainer';
+import TestContainer from './containers/TestContainer';
+
+const store = configureStore();
+
+export default class Root extends Component {
+
+  render() {
+    const processENV = process.env.NODE_ENV || "development"
+    return (
+      <div className="customer_online_message">
+        <Provider store={store} >
+          <Router history={history} >
+            <Route path="/client/index" component={RouteHandler}>
+              <IndexRoute component={MainContainer}/>
+            </Route>
+            <Route path="/test" component={TestContainer} />
+          </Router>
+        </Provider>
+      </div>
+    );
+  }
+}
+
+        // {processENV === 'development' && <DebugPanel top right bottom >
+        //   <DevTools store={store} monitor={LogMonitor} />
+        // </DebugPanel>}
+
 ReactDOM.render(
-  <Root history={history} />,
+  <Root />,
   document.getElementById('app')
 );
