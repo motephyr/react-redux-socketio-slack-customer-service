7055bb09e25da4cab83d0b339d4135f41bc154ef
motephyr <motephyr@gmail.com>
2015-10-27 18:14:59 +0800

    push

--- client/src/app.js
+++ client/src/app.js
@@ -1,21 +1,21 @@
 require('../css/app.css');
-import React from 'react';
+
 import ReactDOM from 'react-dom';
 
 import createBrowserHistory from 'history/lib/createBrowserHistory'
-
 const history = createBrowserHistory();
 
 import {Component, PropTypes} from 'react';
 import {Router, RouteHandler, IndexRoute, Route } from "react-router";
-import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
 
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
 import {Provider} from 'react-redux';
 import configureStore from './store/configureStore';
+const store = configureStore();
+
 import MainContainer from './containers/MainContainer';
 import TestContainer from './containers/TestContainer';
 
-const store = configureStore();
 
 export default class Root extends Component {
 
@@ -24,18 +24,19 @@ export default class Root extends Component {
     return (
       <div className="customer_online_message">
         <Provider store={store} >
-          <Router history={history} >
-            <Route path="/client/index.html" component={RouteHandler}>
-              <IndexRoute component={MainContainer}/>
-            </Route>
-            <Route path="/test" component={TestContainer} />
-          </Router>
+          <MainContainer />
         </Provider>
       </div>
     );
   }
 }
 
+          // <Router history={history} >
+          //   <Route path="/client/index.html" component={RouteHandler}>
+          //     <IndexRoute component={MainContainer}/>
+          //   </Route>
+          //   <Route path="/test" component={TestContainer} />
+          // </Router>
         // {processENV === 'development' && <DebugPanel top right bottom >
         //   <DevTools store={store} monitor={LogMonitor} />
         // </DebugPanel>}
