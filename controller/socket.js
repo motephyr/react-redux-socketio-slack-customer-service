7d79dc3f54fce6b3e1b91e460adf3d140f23a24f
KinoLien <miecowbai@gmail.com>
2016-03-19 16:06:21 +0800

    Add the key mappings rules; If a user cannot re-connect in 8 seconds, the user maybe has left; Avoid duplicate objects in the case of the same domain of multiple tabs.

--- controller/socket.js
+++ controller/socket.js
@@ -2,71 +2,104 @@ var helper = require('./helper');
 var UserIds = require('./userIds');
 
 var domainToUsers = {};
+// {
+//   "google.com": {
+//     "ceid1":[socketobject, ...],
+//     "ceid2":[socketobject, ...],
+//     ...
+//   },
+//   "yahoo.com": {
+//     "ceid2":[socketobject, ...],
+//     ...
+//   },
+//   ...
+// }
+
+var socketCache = {};
+// { "ceid1-google.com": socketObject }
 // client 
 
 module.exports = function (socket, io) {
 
-  // var currentSocketIoUserId = UserIds.create(socket.request.session['user_id']);
-  // UserIds.setSocketIdToUserId(currentSocketIoUserId, socket.id);
-
-  // require('./application_controller')(socket, io);
-  // // require('./write_controller')(socket, io);
-
-  // socket.on('new_email_on_suid', function(obj){
-  //   UserIds.setSocketIdToUserId(obj.email, socket.id);
-  // });
-
-  // server_control_action('new_message');
-
-  // function server_control_action(action) {
-  //   socket.on(action, function (msg) {
-  //     emit_to_customerservice_and_self(action, msg);
-  //   });
-  // }
-
-  // function emit_to_customerservice_and_self(action, msg) {
-  //   // var channel = slack_login.getChannelGroupOrDMByID('C0C1MCEMA');
-  //   // channel.send('收到: ' + msg.name + '傳來的訊息: ' + msg.text);
-
-  //   helper.emitUserId(msg.name, function (x) {
-  //     io.to(x).emit(action, msg);
-  //   });
-  // }
-
-  // helper.emitUserId('0', function (x) {
-  //   io.to(x).emit('client_connected', UserIds.get_connection_status());
-  // });
-
   // room string concat rule: domain and serial number
 
   var defaultRoom = socket.currentDomain + '-public';
-  var joinedRoom = socket.joinedRoom;
+  // var joinedRoom = socket.joinedRoom;
 
-  var map = domainToUsers[socket.currentDomain];
+  var ceidToSocketArray = domainToUsers[socket.currentDomain];
 
-  // check the users array by specified domain
-  if(!map) map = domainToUsers[socket.currentDomain] = [];
+  if(!socket.ceid){
+    // socket.ceid
+    var req = socket.request;
+    var currentCookies = req.headers? req.headers.cookie.split(';') : [];
+    while(currentCookies.length){
+      var check = currentCookies.pop();
+      var parts = check.match(/(.*?)=(.*)$/);
+      if(parts[1].trim() == "_ce"){
+        socket.ceid = (parts[2] || '').trim();
+        break;
+      }
+    }
+  }
+
+  var cacheKey = socket.ceid + '-' + socket.currentDomain;
+
+  // init rooms collection
+  if(!socket.joinedRooms) socket.joinedRooms = [];
 
   if(!socket.username){
     socket.username = 'anonymous_' + (new Date()).getTime();
     socket.emit('change_name', { new_name: socket.username });
   }
 
-  if(map.indexOf(socket.username) == -1){
-    // new user
-    map.push(socket.username);
+
+  // check the users maps by specified domain
+  if(!ceidToSocketArray) ceidToSocketArray = domainToUsers[socket.currentDomain] = {};
+
+  // check if the unique id and socket object mapping
+  if(!ceidToSocketArray[socket.ceid] && !socketCache[cacheKey]){
+
+    ceidToSocketArray[socket.ceid] = [socket];
+
+    var outputUserList = [];
+    for(var id in ceidToSocketArray){
+      var cs = ceidToSocketArray[id][0];
+      // outputUserList.push({
+      //   id: cs.ceid,
+      //   username: cs.username
+      // });
+      outputUserList.push(cs.username);
+    }
+
     socket.join(defaultRoom);
-    socket.emit("initial_list", { room: defaultRoom, users: map });
-    // tell others 
+    socket.joinedRooms.push(defaultRoom);
+    socket.emit("initial_list", { room: defaultRoom, users: outputUserList });
     io.in(defaultRoom).emit('user_joined', {username: socket.username});
+
+  }else{
+    // open new tab but access same domain OR
+    // refresh page / redirect to other pages on same domain
+    var socketsArray = ceidToSocketArray[socket.ceid];
+    var previous = (socketsArray && socketsArray.length > 0)? 
+      socketsArray[socketsArray.length - 1] : socketCache[cacheKey];
+
+    socket.joinedRooms = previous.joinedRooms.slice(0);
+    socket.joinedRooms.forEach(function(r){
+      socket.join(r);
+    });
+
+    if(socketsArray) socketsArray.push(socket);
+    else ceidToSocketArray[socket.ceid] = [socket];
+    
+    delete socketCache[cacheKey];
+    // socketCache[cacheKey] = null;
   }
 
-  if(joinedRoom){
-    socket.join(joinedRoom);
-    socket.broadcast.to(joinedRoom).emit('user_joined', {username: socket.username}); 
-    // io.in(joinedRoom).emit('user_joined', {username: socket.username});
-  }
-  
+  console.log("Socket connected! [" + socket.id + " in " + socket.currentDomain + "]");
+  console.log(domainToUsers);
+  console.log(socketCache);
+  console.log("Info: " + socket.ceid + "; " + socket.username);
+
   socket.on('change_name', function(data){
     // Find the rooms involved by current user 
     var rooms = socket.rooms;
@@ -87,6 +120,15 @@ module.exports = function (socket, io) {
   // means dbclick the another user
   socket.on('create_room', function(data){
     // Find the room involved by current user and target users  (data.targetUsers : Array)
+    var users = data.targetUsers;
+    
+    // socket.join('test room');
+    // var clients = io.sockets.adapter.rooms['test room'];
+    // console.log(clients);
+    // for (var clientId in clients) {
+    //   console.log(io.sockets.connected[clientId]);
+    // }
+
     // if yes
     // ...
     // if not
@@ -103,22 +145,38 @@ module.exports = function (socket, io) {
   });
 
   socket.on('disconnect', function (message) {
-    var joinedRooms = socket.rooms; // rooms is empty? 
-    joinedRooms.forEach(function(name){
-      io.in(name).emit("user_left", {username: socket.username});
-    });
+    // console.log(socket.currentDomain);
+    // console.log(socket.rooms);
+    // var joinedRooms = socket.rooms; // rooms is empty? 
+    
+    // remove
+    var idx = ceidToSocketArray[socket.ceid].map(function(item){ return item.id; }).indexOf(socket.id);
+    ceidToSocketArray[socket.ceid].splice(idx, 1);
+    if(ceidToSocketArray[socket.ceid].length == 0){
+      socketCache[cacheKey] = socket;
+      setTimeout((function(key){
+        return function(){
+          var scope = socketCache[key];
+          if(scope){
+            scope.joinedRooms.forEach(function(name){
+              io.in(name).emit("user_left", {id: scope.ceid, username: scope.username});
+            });
+            delete socketCache[key];
+            // socketCache[key] = null;
 
+            console.log(socketCache);
+          }
+        };
+      })(cacheKey), 8000);
+      delete ceidToSocketArray[socket.ceid];
+      // ceidToSocketArray[socket.ceid] = null;
+    }
 
-    var index = map.indexOf(socket.username);
-    if(index != -1) map.splice(index, 1);
+    console.log("Socket disconnect! [" + socket.id + " in " + socket.currentDomain + "]");
+    console.log(domainToUsers);
+    console.log(socketCache);
+    console.log("Info: " + socket.ceid + "; " + socket.username);
 
-    console.log("which user id " + socket.username + " in " + socket.currentDomain);
-    console.log("which socket id " + socket.id);
-
-    // UserIds.free(currentSocketIoUserId,socket.id);
-    // helper.emitUserId('0', function (x) {
-    //   io.to(x).emit('client_connected', UserIds.get_connection_status());
-    // });
   });
 
 }
